abfa97b7d85314b709a53b8238b5d60171f569b0
Ming <mingdu2015@u.northwestern.edu>
2017-03-27 17:24:09 -0500

    debug

--- sample.py
+++ sample.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from xraylib import *
+from constants import *
+try:
+    from xraylib import *
+    lib_imp = True
+except:
+    lib_imp = False
 
 class Sample(object):
 
@@ -21,5 +26,9 @@ class Sample(object):
         :param energy: energy in keV
         :return: linear attenuation coefficient in um^-1
         """
+        if lib_imp:
+            res = CS_Total_CP(self.formula, energy) * self.density * 1e-4
+        else:
+            res = Mu * Rho * 1e-4
 
-        return CS_Total_CP(self.formula, energy) * self.density * 1e-4
+        return res
