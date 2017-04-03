bb3cae5a37ab6b11805c388bd81b01448737bb17
Ming <mingdu2015@u.northwestern.edu>
2017-04-03 17:14:52 -0500

    debug

--- equal_incident_flux.py
+++ equal_incident_flux.py
@@ -34,12 +34,13 @@ if __name__ == '__main__':
     ds_local = []
     n_proj_full = prj_tomosaic.simulators[0].raw_sino.shape[0]
     for sim in prj_tomosaic.simulators:
-        n_proj_local = int(float(prj_tomosaic.simulators[0].raw_sino.shape[0]) * n_pos_tomosaic / n_pos_local)
+        print(sim.raw_sino.shape[0], n_pos_tomosaic, n_pos_local)
+        n_proj_local = int(sim.raw_sino.shape[0] * n_pos_tomosaic / float(n_pos_local))
         ds_local.append(float(n_proj_full) / float(n_proj_local))
     print ds_local
 
     prj_local = Project()
-    prj_tomosaic.add_simuators(os.path.join('data', 'shepp_sino_trans.tiff'), inst, center=2048, pixel_size=3.2,
+    prj_local.add_simuators(os.path.join('data', 'shepp_sino_trans.tiff'), inst, center=2048, pixel_size=3.2,
                                downsample=ds_local)
 
     prj_tomosaic.process_all_tomosaic()
