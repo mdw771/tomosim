0973d58d849c92d0833f4150bbf230484e1db6d7
Ming <mingdu2015@u.northwestern.edu>
2017-04-04 17:57:09 -0500

    normalize

--- equal_incident_flux.py
+++ equal_incident_flux.py
@@ -67,10 +67,10 @@ if __name__ == '__main__':
     for sim in prj_tomosaic.simulators:
         influx.append(sim.estimate_dose_rough(25.7, sample, np.sqrt(1.779e13), 30, mode='tomosaic')[0])
         img = dxchange.read_tiff(os.path.join('data', 'recon_tomosaic_{:s}x.tiff'.format(sim.name_ds)))
-        snr_tomosaic.append(snr(img, ref_recon, mask_ratio=0.95))
+        snr_tomosaic.append(snr(img, ref_recon, mask_ratio=0.8))
     for sim in prj_local.simulators[1:]:
         img = dxchange.read_tiff(os.path.join('data', 'recon_local_{:s}x.tiff'.format(sim.name_ds)))
-        snr_local.append(snr(img, ref_recon, mask_ratio=0.95))
+        snr_local.append(snr(img, ref_recon, mask_ratio=0.8))
     plt.figure()
     plt.semilogx(influx, snr_local, label='Local', marker='o')
     plt.semilogx(influx, snr_tomosaic, label='Tomosaic', marker='o')
