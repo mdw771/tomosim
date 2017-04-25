c918341e07d4b555005f267d2404bf6e49836949
Ming <mingdu2015@u.northwestern.edu>
2017-04-25 17:57:44 -0500

    poisson

--- sinogram.py
+++ sinogram.py
@@ -78,16 +78,16 @@ class Sinogram(object):
         flag = False
         temp = np.copy(self.sinogram)
         if np.isclose(temp.max(), 1):
-            temp = temp * 1000.
+            temp = temp * 100000.
             flag = True
         x_ref_norm_sq = np.linalg.norm(temp) ** 2
         lam = x_ref_norm_sq / pow(10., snr/10.) / temp.size
-        if flag:
-            lam /= 1.e6
+        # if flag:
+        #     lam /= 1.e6
         noise = np.random.poisson(lam=lam, size=temp.shape) - lam
         self.sinogram = temp + noise
         if flag:
-            self.sinogram = self.sinogram / 1000.
+            self.sinogram = self.sinogram / 100000.
 
     def correct_abs_intensity(self, ref):
 
